ced4ac8fd97cf3cb57578cd9fb1864c8f4dff3f4
aineesh454 <aineeshd@gmail.com>
2026-06-01 19:31:02 +0530

    first commit

---
+++ lesson-1-intro/act1.py
@@ -0,0 +1,4 @@
+print("hello,world!")
+print("my name is aineesh")
+print("i like playing football")
+print("the result of 3+5 is",3+5)
---
+++ lesson-1-intro/act2.py
@@ -0,0 +1,6 @@
+print("welcome to the world of Python!")
+print(4)
+print("Hello python\n")
+print("key words in python are")
+import keyword
+print(keyword.kwlist)
